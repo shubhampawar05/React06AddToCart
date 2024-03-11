bc98ca9ed1c7e5e99ae9f5110ddb18ace58fa0e3
Shubham Chopde <shubhamchopde090@gmail.com>
2024-03-11 11:41:46 +0530

    final working

--- src/Componetes/SingleCart.jsx
+++ src/Componetes/SingleCart.jsx
@@ -11,11 +11,16 @@ function SingleCart({ Data, quantity, setQuantity,setProduct, product,setTotal ,
         items.quantity+=1
         setCartValue(cartvalue+1)
         setQuantity( quantity+1)
-        setTotal( total + items.quantity*items.price)
+        setTotal( total + items.price)
       }
     }
 
     )
+    // setProduct((previous)=>{
+    //   let newproducts = [...previous]
+    //   newproducts[id-1].quantity++
+    //   return newproducts
+    // })
   };
 
   const Decresehandler = (id) => {
@@ -26,7 +31,7 @@ function SingleCart({ Data, quantity, setQuantity,setProduct, product,setTotal ,
           items.quantity-= 1
           setCartValue(cartvalue-1)
           setQuantity( quantity-1)
-          setTotal( total - (items.quantity*items.price))
+          setTotal( total - (items.price))
         }
         
       }
@@ -76,7 +81,7 @@ function SingleCart({ Data, quantity, setQuantity,setProduct, product,setTotal ,
             <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
           </svg>
         </span>
-        <span className="pl-1">{cartvalue}</span>
+        <span className="pl-1">{Data.quantity}</span>
         <span>
           <svg
             stroke="currentColor"
